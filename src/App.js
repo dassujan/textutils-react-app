3fd7b18ea0c66381653b5018c08dce8d94ef40d2
“dassujan” <dassujan.005@gmail.com>
2021-08-26 21:45:01 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -8,8 +9,8 @@ function App() {
     {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
     <Navbar title="TextUtils"/>
     <div className="container my-3">
-
     <TextForm heading="Enter the text to analyze below"/>
+    {/* <About/> */}
     </div>
     {/* <Navbar/> */}
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,10 +14,29 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText)
     }
+    
+    const handleclearClick = () => {
+        // console.log("Clear Text was clicked: " + text);
+        let newText = '';
+        setText(newText)
+    }
 
     const handleOnChange = (event) => {
         // console.log("On Change");
         setText(event.target.value)
+    }
+
+    const handleCopy = () => {
+        // console.log("Text is copied");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () => {
+        // console.log("Extra Spaces is Removed");
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
     }
 
     const [text, setText] = useState('');
@@ -33,6 +52,9 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handlelowClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleclearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
         <div className="container my-3">
             <h2>Your text summary</h2>
